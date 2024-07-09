02b25d3197d659c1097bd94ca59c0cc752da2c72
michael <75301804+mikartisan@users.noreply.github.com>
2024-07-09 15:26:00 +0800

    Update database connection

--- database/connect.php
+++ database/connect.php
@@ -1,12 +1,3 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '','courier_db') or die("Connection failed: " . mysqli_connect_error());
-	// $conn = mysqli_connect('localhost', 'id20122348_courier', '2vdcp_?^YTI6*Y]x','id20122348_courier_db') or die("Connection failed: " . mysqli_connect_error());
-	try{
-        $pdo = new PDO("mysql:host=localhost;dbname=courier_db", "root", "");
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } 
-    catch(PDOException $e){
-        die("ERROR: Could not connect. " . $e->getMessage());
-    }
-    
+    $conn = mysqli_connect('localhost', 'root', '','todar_db') or die("Connection failed: " . mysqli_connect_error());
 ?> 
